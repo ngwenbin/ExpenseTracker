ea7d7701b7db01acc32b35d3bc031a92330fa9e7
ngwenbin <wenbin_ng@yahoo.com.sg>
2020-03-23 15:43:04 +0800

    Update

--- app.py
+++ app.py
@@ -136,16 +136,23 @@ def userexpenses(wkex_id):
     name = current_user.username
     items = db.session.query(UserExpense).filter(UserExpense.userid==userids, UserExpense.expense_date==wkex_id)
 
+    state='not'
+
+    todays = date.today()
+    if wkex_id == todays:
+        state='today'
+
     if form.validate_on_submit():
         expenses = UserExpense(category=form.category.data, description=form.description.data,
                                 expense=form.expense.data, expensedate=current_user)
+
         db.session.add(expenses)
         db.session.commit()
 
         flash('Expense added!', 'success')
         return redirect(url_for('userexpenses', wkex_id=wkex_id))
     return render_template('expenses.html', normal='normal', title='Expenses',
-                           form=form, items=items, name=name, ids=wkex_id)
+                           form=form, items=items, name=name, ids=wkex_id, state=state)
 
 @app.route('/expense/<string:wkex_id>/<int:ex_id>/delete', methods=['GET','POST'])
 @login_required
